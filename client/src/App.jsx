e3a377e53ec28bb8daf6a9ec541a77235d8953b8
user.name like : praveenkumarbermo@gamil.com <praveenkumarbermo@gamil.com>
2025-08-24 19:52:03 +0530

    feat: Add emailjs dependency and implement protected routes for user authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-
 import './App.css'
 import Login from './pages/auth/Login'
 import Signup from './pages/auth/Signup.jsx'
-import {Routes , Route} from "react-router-dom"
-import Navbar from './components/Navbar.jsx'
+import { Routes, Route, Navigate, useLocation } from "react-router-dom"
+import { useEffect } from "react"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
 import Profile from './pages/dashboard/Profile.jsx'
 import Home from './pages/home/Home.jsx'
 import VideoPlayer from './pages/videoPlayer/VideoPlayer.jsx'
@@ -18,35 +20,56 @@ import SearchResults from './pages/searchResults/SearchResults.jsx'
 import ChannelDashboard from './pages/dashboard/ChannelDashboard.jsx'
 import Support from './pages/support/Support.jsx'
 
+import { useAuth } from './context/AuthContext.jsx'
 
+//Protected Route
+function ProtectedRoute({ children }) {
+  const { user } = useAuth();
 
+  if (!user) {
+    return <Navigate to="/login" replace state={{ message: "Please login first to continue." }} />;
+  }
 
+  return children;
+}
 
 function App() {
+  const location = useLocation();
+
+  // Show toast if redirected with message
+  useEffect(() => {
+    if (location.state?.message) {
+      toast.error(location.state.message, {
+        position: "top-right",
+        autoClose: 3000,
+        theme: "dark",
+      });
+    }
+  }, [location]);
 
   return (
     <>
-
+      <ToastContainer />
       <Routes>
-        //root router
-        <Route path= "/" element = {<Home />} />
+        {/* Public routes */}
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path = "/dashboard/profile" element = {<Profile />} />
-        <Route path="/video/:videoId" element = {<VideoPlayer />} />
-        <Route path='/likedVideos' element={<LikedVideos />} />
-        <Route path= '/publishedVideos' element = {<PublishedVideos />} />
-        <Route path="/publishPage" element={<PublishPage />} />
-        <Route path='/subscribedChannelList' element={<SubscribedChannelsPage />} />
-        <Route path='/playlist' element={<PlaylistsPage />} />
-        <Route path='/playlist/:playlistId' element={<PlaylistDetailsPage />} />
-        <Route path = '/updateUserDetails' element={<UpdateUserDetails />} />
-        <Route path='/search' element ={<SearchResults />} />
-        <Route path = '/channelDashboard/:channelId' element={<ChannelDashboard />} />
-        <Route path='/contact' element={<Support />} />
-        
-      </Routes>
 
+        {/* Protected routes */}
+        <Route path="/dashboard/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="/video/:videoId" element={<ProtectedRoute><VideoPlayer /></ProtectedRoute>} />
+        <Route path="/likedVideos" element={<ProtectedRoute><LikedVideos /></ProtectedRoute>} />
+        <Route path="/publishedVideos" element={<ProtectedRoute><PublishedVideos /></ProtectedRoute>} />
+        <Route path="/publishPage" element={<ProtectedRoute><PublishPage /></ProtectedRoute>} />
+        <Route path="/subscribedChannelList" element={<ProtectedRoute><SubscribedChannelsPage /></ProtectedRoute>} />
+        <Route path="/playlist" element={<ProtectedRoute><PlaylistsPage /></ProtectedRoute>} />
+        <Route path="/playlist/:playlistId" element={<ProtectedRoute><PlaylistDetailsPage /></ProtectedRoute>} />
+        <Route path="/updateUserDetails" element={<ProtectedRoute><UpdateUserDetails /></ProtectedRoute>} />
+        <Route path="/search" element={<ProtectedRoute><SearchResults /></ProtectedRoute>} />
+        <Route path="/channelDashboard/:channelId" element={<ProtectedRoute><ChannelDashboard /></ProtectedRoute>} />
+        <Route path="/contact" element={<ProtectedRoute><Support /></ProtectedRoute>} />
+      </Routes>
     </>
   )
 }
